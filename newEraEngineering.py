690ca94839ad26368b463b188ed0d411fdda078a
gopalsarkar12 <110389254+gopalsarkar12@users.noreply.github.com>
2022-08-01 17:30:38 +0530

    Add files via upload

---
+++ newEraEngineering.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+pd.read_csv("NewEraEngineering.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
